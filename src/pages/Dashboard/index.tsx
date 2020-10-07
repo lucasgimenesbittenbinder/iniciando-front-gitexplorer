f1a03e9a265177cabcfef9e6845a77662a3cb2b9
Lucas Gimenes <lucas.gimenesbittenbinder@gmail.com>
2020-10-06 22:48:46 -0300

    feat: estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/59899836?s=460&u=7e0242ab55550e235d55f0ba674ec08123f5ddbc&v=4"
+            alt="Lucas Gimenes"
+          />
+          <div>
+            <strong>lucas.gimenesbittenbinder</strong>
+            <p>Bem-vindo ao meu primeiro repositório com o React.JS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/59899836?s=460&u=7e0242ab55550e235d55f0ba674ec08123f5ddbc&v=4"
+            alt="Lucas Gimenes"
+          />
+          <div>
+            <strong>lucas.gimenesbittenbinder</strong>
+            <p>Bem-vindo ao meu primeiro repositório com o React.JS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/59899836?s=460&u=7e0242ab55550e235d55f0ba674ec08123f5ddbc&v=4"
+            alt="Lucas Gimenes"
+          />
+          <div>
+            <strong>lucas.gimenesbittenbinder</strong>
+            <p>Bem-vindo ao meu primeiro repositório com o React.JS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
